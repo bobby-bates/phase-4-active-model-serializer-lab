e42c5ccaa8f52bf76bcae399f2c548ce2ad182d4
Bobby Bates <robertfbatesjr@gmail.com>
2022-09-04 18:34:02 -0400

    complete assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,15 +3,28 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all
-    render json: authors
+    # byebug
+    # render json: authors
+    # render json: authors, each_serializer: AuthorProfileSerializer
+    render json: authors, include: ['profile', 'posts', 'posts.tags']
+  end
+  
+  def show
+    author = find_author
+    # render json: author
+    render json: author, include: ['profile', 'posts', 'posts.tags']
   end
 
-  def show
-    author = Author.find(params[:id])
-    render json: author
+  def profile
+    author = find_author
+    render json: author, serializer: AuthorProfileSerializer
   end
 
   private
+
+  def find_author
+    Author.find(params[:id])
+  end
 
   def render_not_found_response
     render json: { error: "Author not found" }, status: :not_found
